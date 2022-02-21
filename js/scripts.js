6f018013719cc4942ba595f4df24cf304b3300dc
robertduplock <robertduplock@users.noreply.github.com>
2022-02-21 21:49:05 +0000

    deploy: b8cf4fffcfa5c4f3526d23cf54778bcccdf23360

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+/**
+ * Expands and contracts the articles when the readmore link is clicked.
+ */
+const readMore = () => {
+    const readMoreLinks = document.getElementsByClassName('read-more');
+    const showLessLinks = document.getElementsByClassName('show-less');
+
+    for (let i = 0; i < readMoreLinks.length; i++) {
+        const link = readMoreLinks[i];
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const article = link.closest('article');
+            article.querySelector('.full-content').classList.add('show');
+            article.querySelector('.read-more').classList.add('dn');
+            article.querySelector('.show-less').classList.remove('dn');
+        })
+    }
+
+    for (let i = 0; i < showLessLinks.length; i++) {
+        const link = showLessLinks[i];
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const article = link.closest('article');
+            article.querySelector('.full-content').classList.remove('show');
+            article.querySelector('.read-more').classList.remove('dn');
+            article.querySelector('.show-less').classList.add('dn');
+        })
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    readMore();
+});
